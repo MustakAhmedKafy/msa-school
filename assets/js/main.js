252abf25a1db9e961535097952c919961bd4e04d
Mustak Ahmed Kafy <ahmedmustak99@gmail.com>
2024-08-31 00:57:12 +0600

    page add

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ $("#most-viewd-items").owlCarousel({
   loop: true,
   autoplay: true,
   nav: true,
-  dots: false,
+  dots: false, // Default to false
   navText: [
     '<span class="iconify" data-icon="bi:arrow-left-short" data-width="24" data-height="24"></span>',
     '<span class="iconify" data-icon="bi:arrow-right-short" data-width="24" data-height="24"></span>',
@@ -42,22 +42,29 @@ $("#most-viewd-items").owlCarousel({
     0: {
       items: 1,
       margin: 10,
+      nav: false, // Hide nav on mobile
+      dots: true, // Show dots on mobile
     },
     600: {
       items: 2,
       margin: 15,
+      nav: false, // Hide nav on small screens
+      dots: true, // Show dots on small screens
     },
     1000: {
       items: 3,
       margin: 20,
+      nav: true, // Show nav on larger screens
+      dots: false, // Hide dots on larger screens
     },
   },
 });
+
 $("#home-testimonial").owlCarousel({
   loop: true,
   autoplay: true,
   nav: true,
-  dots: false,
+  dots: false, // Default to false
   navText: [
     '<span class="iconify" data-icon="bi:arrow-left-short" data-width="24" data-height="24"></span>',
     '<span class="iconify" data-icon="bi:arrow-right-short" data-width="24" data-height="24"></span>',
@@ -66,14 +73,20 @@ $("#home-testimonial").owlCarousel({
     0: {
       items: 1,
       margin: 10,
+      nav: false, // Hide nav on mobile
+      dots: true, // Show dots on mobile
     },
     600: {
       items: 2,
       margin: 15,
+      nav: false, // Hide nav on small screens
+      dots: true, // Show dots on small screens
     },
     1000: {
       items: 4,
       margin: 20,
+      nav: true, // Show nav on larger screens
+      dots: false, // Hide dots on larger screens
     },
   },
 });
